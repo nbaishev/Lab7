6e67c45831f3b9e4e8a975c4bffdd2518a27e6b4
Николай Баишев <baishev.nk@stud.s-vfu.ru>
2022-01-09 17:30:41 +0900

    Add project files.

---
+++ Task7/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длины сторон первого треугольника");
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            int b1 = Convert.ToInt32(Console.ReadLine());
+            int c1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите длины сторон второго треугольника");
+            int a2 = Convert.ToInt32(Console.ReadLine());
+            int b2 = Convert.ToInt32(Console.ReadLine());
+            int c2 = Convert.ToInt32(Console.ReadLine());
+
+            if (TriangleSquare(a1, b1, c1) > TriangleSquare(a2, b2, c2))
+            {
+                Console.WriteLine("Площадь первого треугольника больше");
+            }
+
+            if (TriangleSquare(a1, b1, c1) < TriangleSquare(a2, b2, c2))
+            {
+                Console.WriteLine("Площадь второго треугольника больше");
+            }
+
+            if (TriangleSquare(a1, b1, c1) == TriangleSquare(a2, b2, c2))
+            {
+                Console.WriteLine("Площади равны");
+            }
+
+            Console.ReadKey();
+        }
+
+        static double TriangleSquare(int a, int b, int c)
+        {
+            double p = (a + b + c) / 2;
+            double s = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            return s;
+        }
+    }
+}
---
+++ Task7_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину ребра куба");
+            int a = Convert.ToInt32(Console.ReadLine());
+            CubeProp(a);
+            Console.ReadKey();
+        }
+
+        static void CubeProp(int a)
+        {
+            int S = a * a * 6;
+            int V = a * a * a;
+            Console.WriteLine("Площадь поверхности куба {0}, объем {1}", S, V);
+        }
+    }
+}
